ea8cd97f7638cd9ad6209a410dad612a41de6b2e
Ivan <atknini@yahoo.com>
2023-12-06 03:47:39 -0800

    e

--- start.py
+++ start.py
@@ -18,7 +18,7 @@ from seleniumbase import Driver
 
 from write import to_excel
 ua = UserAgent()
-headless = True
+headless = False
 driver = Driver(uc=True, incognito=True, proxy=proxy(), agent  = ua.random,headless=headless)
 # меняем IP адрес при старте
 requests.get('https://changeip.mobileproxy.space/?proxy_key=c9d64935f5f935255181a3ee425e83bd')
@@ -43,7 +43,7 @@ for el in comp_input:
         driver.quit()
         time.sleep(2)
         ua = UserAgent()
-        driver = Driver(uc=True, incognito=True, proxy=proxy_server_full, agent  = ua.random)
+        driver = Driver(uc=True, incognito=True, proxy=proxy(), agent  = ua.random)
 
 
 # записать все в файл
