c1e760a5eab5068a7a13e5534a7e855303f84a91
Ivan <atknini@yahoo.com>
2024-06-01 17:47:09 +0800

    e

--- close_after_time_js.js
+++ close_after_time_js.js
@@ -3,6 +3,6 @@
         function() {
             // if($('html:contains("Ошибка 429")').length > 0)  {close_after_time({'error': 'error 429'})};
             close_after_time(document.documentElement.outerHTML);
-        }, 30000 );
+        }, 40000 );
         
 })();
